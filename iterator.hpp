9be950634e5403e1e8fe27cc6d62d9000eef4a9b
Chang hyun Jang <cjang@student.42seoul.kr>
2022-07-30 03:11:45 +0900

    Add reverse_iterator, is_integral_base contents
    reverse_iterator, is_integral_base 헤더
    내용 추가

--- iterator.hpp
+++ iterator.hpp
@@ -1,4 +1,4 @@
-//#include <iterator>
+// #include <iterator>
 #include <cstddef>	//std::ptrdiff_t
 
 namespace ft
@@ -64,21 +64,19 @@ namespace ft
 		typedef ft::random_access_iterator_tag		iterator_category;
 	};
 
-/*
-	std::reverse_iterator
-	iterator adaptor for reverse-order traversal
-	(class template)
-*/
+	/* std::reverse_iterator : iterator adaptor for reverse-order traversal (class template) */
+	// current는 기본 iterator
 	template <class Iter>
 	class reverse_iterator
 		: public ft::iterator<
-			ft::iterator_traits<Iter>::iterator_traits<Iter>::iterator_category,
+			ft::iterator_traits<Iter>::iterator_category,
 			ft::iterator_traits<Iter>::value_type,
 			ft::iterator_traits<Iter>::difference_type,
 			ft::iterator_traits<iTer>::pointer,
 			ft::iterator_traits<Iter>::reference,
 		>
 	{
+	public:
 		typedef Iter											iterator_type;
 		typedef ft::iterator_traits<Iter>::iterator_category	iterator_category;
 		typedef ft::iterator_traits<Iter>::value_type			value_type;
@@ -86,36 +84,53 @@ namespace ft
 		typedef ft::iterator_traits<Iter>::pointer				pointer;
 		typedef ft::iterator_traits<Iter>::reference			reference;
 
-	public:
 		/* (constructor) : constructs a new iterator adaptor */
 		/* std::reverse_iterator<Iter>:reverse_iterator */
-		reverse_iterator();
-		explicit reverse_iterator(iterator_type x);
+		reverse_iterator() : current() { }
+
+		explicit reverse_iterator(iterator_type x) : current(x) { }
+
 		template <class U>
-		reverse_iterator(const reverse_iterator<U>& othrer);
+		reverse_iterator(const reverse_iterator<U>& other) : current(other.base()) { }
 
 		/* operator= : assigns another iterator adaptor */
 		/* std::reverse_iterator<Iter>::operator= */
 		template <class U>
-		reverse_iterator& operator=(const reverse_iterator<U>& other);
+		reverse_iterator& operator=(const reverse_iterator<U>& other)
+		{
+			this->current = other->base();
+			return *this;
+		}
 
 		/* base : accesses the underlying iterator */
 		/* (즉, iterator로 돌아온다고 생각하면 될듯) */
 		/* std::reverse_iterator<Iter>::base */
-		iterator_type base() const;
+		iterator_type base() const
+		{
+			return current;
+		}
 
-		/*
-			operator*, operator-> :
-			access the pointed-to element
-		*/
+		/* operator*, operator-> : access the pointed-to element */
 		/* std::reverse_iterator<Iter>::operator* */
 		/* std::reverse_iterator<Iter>::operator-> */
-		reference operator*() const;
-		pointer operator->() const;
+		// -1하는건 reverse라서
+		reference operator*() const
+		{
+			Iter tmp = current;
+			return *--tmp;
+		}
+
+		pointer operator->() const
+		{
+			return &(operator*());
+		}
 
 		/* operator[] : accesses an element by index */
 		/* std::reverse_iterator<Iter>::operator[] */
-		/* unspecified */ operator[] (difference_type n) const;
+		reference operator[] (difference_type n) const
+		{
+			return *(*this + n);
+		}
 
 
 		/* advances or decrements the iterator */
@@ -124,72 +139,126 @@ namespace ft
 		/* pre_increments or pre_decrements by one respectively */
 
 		/* operator++ */
-		reverse_iterator& operator++();
+		//
+		reverse_iterator& operator++()
+		{
+			--current;
+			return *this;
+		}
 
 		/* operator-- */
-		reverse_iterator& operator--();
+		reverse_iterator& operator--()
+		{
+			++current;
+			return *this;
+		}
+
+		reverse_iterator operator++(int)
+		{
+			reverse_iterator tmp(*this);
+			--current;
+			return tmp;
+		}
+
+		reverse_iterator operator--(int)
+		{
+			reverse_iterator tmp(*this);
+			++current;
+			return *this;
+		}
 
 		/* operator+ */
-		reverse_iterator& operator+(difference_type n) const;
+		// 자기자신의 값이 바뀌지 않음 -> *this가 아니라 생성자로 리턴
+		reverse_iterator& operator+(difference_type n) const
+		{
+			return reverse_iterator(this->current - n);
+		}
 
 		/* operator- */
-		reverse_iterator operator-(difference_type n) const;
+		reverse_iterator operator-(difference_type n) const
+		{
+			return reverse_iterator(this->current + n);
+		}
 
 		/* operator+= */
-		reverse_iterator operator+=(diffenece_type n);
+		reverse_iterator operator+=(diffenece_type n)
+		{
+			current -= n;
+			return *this;
+		}
 
 		/* operator-= */
-		reverse_iterator operator-=(difference_type n);
+		reverse_iterator operator-=(difference_type n)
+		{
+			current += n;
+			return *this;
+		}
 
 	protected:
 		/* current(protected) : the underlying iterator of which base() returns a copy */
-		iterator current;
+		Iter current;
 
 	public:
 		/* compares the underlying iterators */
 		/* operator== */
 		template <class Iterator1, class Iterator2>
-		bool operator==(const ft::reverse_iterator<Iterator1>& lhs,
-			const ft:reverse_iterator<Iterator2>& rhs);
+		bool operator==(const ft::reverse_iterator<Iterator1>& lhs, const ft:reverse_iterator<Iterator2>& rhs)
+		{
+			return lhs->base() == rhs->base();
+		}
 
 		/* operator!= */
 		template <class Iterator1, class Iterator2>
-		bool operator!=(const ft::reverse_iterator<Iterator1>& lhs,
-			const ft::reverse_iterator2<Iterator2>& rhs);
+		bool operator!=(const ft::reverse_iterator<Iterator1>& lhs, const ft::reverse_iterator2<Iterator2>& rhs)
+		{
+			return lhs->base() == rhs->base();
+		}
 
 		/* operator< */
 		template <class Iterator1, class Iterator2>
-		bool operator<(const ft::reverse_iterator<Iterator1>& lhs,
-			const ft:reverse_iterator<Iterator2>& rhs);
+		bool operator<(const ft::reverse_iterator<Iterator1>& lhs, const ft:reverse_iterator<Iterator2>& rhs)
+		{
+			return lhs->base() < rhs->base();
+		}
 
 		/* operator<= */
 		template <class Iterator1, class Iterator2>
-		bool operator<=(const ft::reverse_iterator<Iterator1>& lhs,
-			const ft:reverse_iterator<Iterator2>& rhs);
+		bool operator<=(const ft::reverse_iterator<Iterator1>& lhs, const ft:reverse_iterator<Iterator2>& rhs)
+		{
+			return lhs->base() <= rhs->base();
+		}
 
 		/* operator> */
 		template <class Iterator1, class Iterator2>
-		bool operator>(const ft::reverse_iterator<Iterator1>& lhs,
-			const ft:reverse_iterator<Iterator2>& rhs);
+		bool operator>(const ft::reverse_iterator<Iterator1>& lhs, const ft:reverse_iterator<Iterator2>& rhs)
+		{
+			return lhs->base() > rhs->base();
+		}
 
 		/* operator>= */
 		template <class Iterator1, class Iterator2>
-		bool operator>=(const ft::reverse_iterator<Iterator1>& lhs,
-			const ft:reverse_iterator<Iterator2>& rhs);
+		bool operator>=(const ft::reverse_iterator<Iterator1>& lhs, const ft:reverse_iterator<Iterator2>& rhs)
+		{
+			return lhs->base() >= rhs->base();
+		}
 
 		/* operator+ : advances the iterator */
 		/* operator+(std::reverse_iterator) */
 		/* 2 + itr 같이 iterator가 뒤에 오는 경우 */
 		template <class Iter>
 		reverse_iterator<Iter> operator+(
-			typename reverse_iterator<Iter>::difference_type n,
-			const reverse_iterator<Iter>& it );
+			typename reverse_iterator<Iter>::difference_type n, const reverse_iterator<Iter>& it )
+		{
+			return reverse_iterator<Iter>(it.base() - n);
+		}
 
 		/* operator- : conputes the distance beetween two iterator adaptors */
 		/* operator-(std::reverse_iterator) */
 		template <class Iterator>
-		typename reverse_iterator<Iterator>::difference_type
-			operator-(const reverse_iterator<Iterator>& lhs,
-			const reverse_iterator<Iterator>& rhs );
-	}
+		typename reverse_iterator<Iterator>::difference_type operator-(
+			const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs )
+		{
+			return rhs.base() - lhs.base();
+		}
+	};
 }
--- type_traits.hpp
+++ type_traits.hpp
@@ -1,4 +1,4 @@
-// #include <type_traits>
+#include <type_traits>
 
 namespace ft
 {
@@ -9,7 +9,6 @@ namespace ft
 	template <class T>
 	struct enable_if<true, T> { typedef T type; };
 
-	// integral쪽은 고민 필요
 	/*
 		integral_constant : compile-time constant of specified type with specified value
 		class template, since C++11
@@ -33,11 +32,22 @@ namespace ft
 	typedef integral_constant<bool, false>	false_type;
 
 	// is_integral_base를 이용하여 해당하는 integral을 true_type으로 해줌.
-	template <class T>
-	struct is_integral_base : public false_type {};
-
-	template <>
-	struct is_integral_base<int> : public true_type {};
+	template <class T> struct is_integral_base : public false_type {};
+	template <> struct is_integral_base<bool> : public true_type {};
+	template <> struct is_integral_base<char> : public true_type {};
+	template <> struct is_integral_base<signed char> : public true_type {};
+	template <> struct is_integral_base<unsigned char> : public true_type {};
+	template <> struct is_integral_base<char16_t> : public true_type {};
+	template <> struct is_integral_base<char32_t> : public true_type {};
+	template <> struct is_integral_base<wchar_t> : public true_type {};
+	template <> struct is_integral_base<short> : public true_type {};
+	template <> struct is_integral_base<unsigned short> : public true_type {};
+	template <> struct is_integral_base<int> : public true_type {};
+	template <> struct is_integral_base<unsigned int> : public true_type {};
+	template <> struct is_integral_base<long> : public true_type {};
+	template <> struct is_integral_base<unsigned long> : public true_type {};
+	template <> struct is_integral_base<long long> : public true_type {};
+	template <> struct is_integral_base<unsigned long long> : public true_type {};
 
 	/*
 		is_integral : checks if a type is an integral type
